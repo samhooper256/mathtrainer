dc4003245cbde943e32b30f1c58547ee57ec92c2
samho <samho@LAPTOP-U6S44RL2.attlocal.net>
2020-10-26 15:16:13 -0500

    Worked on TimeQueue

--- MathUIL/src/base/TimeQueue.java
+++ MathUIL/src/base/TimeQueue.java
@@ -7,16 +7,18 @@ package base;
 public class TimeQueue {
 	
 	private double[] elements;
+	private final int capacity;
 	private int size;
+	private int head, tail;
 	
 	public TimeQueue(final int capacity) {
-		this.elements = new double[capacity];
+		this.capacity = capacity;
+		this.elements = new double[this.capacity];
 		this.size = 0;
 	}
 	
 	public void add(double item) {
 		if(size == elements.length) {
-			
 			
 		}
 	}
