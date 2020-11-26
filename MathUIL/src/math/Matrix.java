3b9e32707f9904f13efb3ad17e44c5015e2c2e36
samhooper256 <samhooper256@gmail.com>
2020-11-25 18:14:51 -0600

    Added Matrix class

--- MathUIL/src/math/Matrix.java
+++ MathUIL/src/math/Matrix.java
@@ -1,6 +1,7 @@
 package math;
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.function.*;
 
 /**
  * A matrix of {@link BigFraction BigFractions}. Rows and columns are indexed starting from zero. {@code Matrix} object are immutable.
@@ -8,6 +9,14 @@ import java.util.Arrays;
  *
  */
 public class Matrix {
+	
+	public interface CellConsumer {
+		void accept(final int row, final int col);
+	}
+	
+	public interface CellMapper {
+		BigFraction apply(final int row, final int col);
+	}
 	
 	/**
 	 * Returns a new {@link Matrix} whose elements are those in {@code elements}. The given array is defensively copied. {@code elements.length}
@@ -40,6 +49,13 @@ public class Matrix {
 		return result;
 	}
 	
+	/**
+	 * Returns {@code true} if {@code a} and {@code b} have the same dimensions, {@code false} otherwise.
+	 */
+	public static boolean sameDimensions(Matrix a, Matrix b) {
+		return a.rowCount() == b.rowCount() && a.colCount() == b.colCount();
+	}
+	
 	private final BigFraction[][] elements;
 	
 	/**
@@ -49,6 +65,110 @@ public class Matrix {
 		this.elements = elements;
 	}
 	
+	public void forEach(final Consumer<BigFraction> consumer) {
+		Objects.requireNonNull(consumer);
+		forEachCell((r, c) -> consumer.accept(get(r, c)));
+	}
+	
+	public void forEachCell(final CellConsumer consumer) {
+		Objects.requireNonNull(consumer);
+		for(int row = 0; row < rowCount(); row++)
+			for(int col = 0; col < colCount(); col++)
+				consumer.accept(row, col);
+	}
+	
+	public Matrix map(UnaryOperator<BigFraction> mapper) {
+		return map((r, c) -> mapper.apply(get(r, c)));
+	}
+	
+	public Matrix map(CellMapper mapper) {
+		BigFraction[][] newElements = new BigFraction[rowCount()][colCount()];
+		forEachCell((r, c) -> newElements[r][c] = mapper.apply(r, c));
+		return new Matrix(newElements);
+	}
+	
+	/**
+	 * Returns a {@link Matrix} where every element is the {@link BigFraction#negate() negation} of the corresponding element in {@code this}
+	 * {@code Matrix}.
+	 */
+	public Matrix negate() {
+		return map(BigFraction::negate);
+	}
+	
+	/**
+	 * Returns {@code (this + val)}. Throws an exception if adding the two matrices is impossible.
+	 * @throws NullPointerException if {@code val} is {@code null}.
+	 * @throws ArithmeticException if {@code this} and {@code val} do not have the {@link #sameDimensions(Matrix, Matrix) same dimensions}.
+	 */
+	public Matrix add(Matrix val) {
+		Objects.requireNonNull(val);
+		if(!sameDimensions(this, val))
+			throw new ArithmeticException("this and val have different dimensions");
+		return map((r, c) -> get(r, c).add(val.get(r, c)));
+	}
+	
+	/**
+	 *Returns {@code (this - val)}. Throws an exception if subtracting the two matrices is impossible.
+	 * @throws NullPointerException if {@code val} is {@code null}.
+	 * @throws ArithmeticException if {@code this} and {@code val} do not have the {@link #sameDimensions(Matrix, Matrix) same dimensions}.
+	 */
+	public Matrix subtract(Matrix val) {
+		Objects.requireNonNull(val);
+		if(!sameDimensions(this, val))
+			throw new ArithmeticException("this and val have different dimensions");
+		return map((r, c) -> get(r, c).subtract(val.get(r, c)));
+	}
+	
+	/**
+	 * Returns {@code (this * val)}. Throws an exception if multiplying the two matrices is impossible.
+	 * @throws NullPointerException if {@code val} is {@code null}.
+	 * @throws ArithmeticException if {@code this} and {@code val} do not have compatible dimensions.
+	 */
+	public Matrix multiply(Matrix val) {
+		Objects.requireNonNull(val);
+		if(rowCount() != val.colCount() || colCount() != val.rowCount())
+			throw new ArithmeticException("this and val do not have compatible dimensions");
+		return map((r, c) -> {
+			BigFraction total = BigFraction.ZERO;
+			for(int i = 0; i < colCount(); i++)
+				total = total.add(get(r, i).multiply(val.get(i, c)));
+			return total;
+		});
+	}
+	
+	/**
+	 * Returns the determinant of this {@link Matrix} as a {@link BigFraction}. Determinants are not supported for
+	 * matrices larger than 2x2.
+	 * @throws ArithmeticException if {@code this} {@link Matrix} is not {@link #isSquare() square}.
+	 * @throws UnsupportedOperationException if this {@code Matrix} is square but is larger than 2x2.
+	 */
+	public BigFraction determinant() {
+		if(!isSquare())
+			throw new ArithmeticException("Cannot find the determinant of a non-square matrix");
+		if(rowCount() > 2)
+			throw new UnsupportedOperationException("Calculating the determinant of a " + rowCount() + "x" + colCount() + " matrix is not supported");
+		if(rowCount() == 1)
+			return get(0, 0);
+		return get(0, 0).multiply(get(1, 1)).subtract(get(0, 1).multiply(get(1, 0)));
+	}
+	
+	public BigFraction sumOfRow(final int row) {
+		BigFraction total = BigFraction.ZERO;
+		for(BigFraction f : elements[row])
+			total = total.add(f);
+		return total;
+	}
+	
+	public BigFraction sumOfCol(final int col) {
+		BigFraction total = BigFraction.ZERO;
+		for(int row = 0; row < rowCount(); row++)
+			total = total.add(elements[row][col]);
+		return total;
+	}
+	
+	/**
+	 * Returns the element at row {@code row} and column {@code col} in this {@link Matrix}.
+	 */
 	public BigFraction get(final int row, final int col) {
 		return elements[row][col];
 	}
@@ -74,11 +194,51 @@ public class Matrix {
 		return column;
 	}
 	
+	/**
+	 * Returns the number of rows in this {@link Matrix}. The returned value will always be greater than {@code 0}.
+	 */
 	public int rowCount() {
 		return elements.length;
 	}
 	
+	/**
+	 * Returns the number of columns in this {@link Matrix}. The returned value will always be greater than {@code 0}.
+	 */
 	public int colCount() {
 		return elements[0].length;
 	}
+	
+	/**
+	 * Returns {@code true} if {@code this} is a square {@link Matrix} (a matrix with the same number of rows as columns), {@code false}
+	 * otherwise.
+	 */
+	public boolean isSquare() {
+		return rowCount() == colCount();
+	}
+
+	@Override
+	public String toString() {
+		return Arrays.deepToString(elements);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Arrays.deepHashCode(elements);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj)
+			return true;
+		if(obj == null)
+			return false;
+		if(getClass() != obj.getClass())
+			return false;
+		Matrix other = (Matrix) obj;
+		return Arrays.deepEquals(elements, other.elements);
+	}
+	
 }
